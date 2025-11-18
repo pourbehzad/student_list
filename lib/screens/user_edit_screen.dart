7183dd734a5276e30863196da19de07117d2bce3
pourbehzad <pourbehzad.ali@gmail.com>
2025-11-18 12:55:26 +0330

    feat(UI): finish ui design of user edit screen

--- lib/screens/user_edit_screen.dart
+++ lib/screens/user_edit_screen.dart
@@ -10,8 +10,81 @@ class UserEditScreen extends StatefulWidget {
 }
 
 class _UserEditScreenState extends State<UserEditScreen> {
+  final GlobalKey<FormState> formkey = GlobalKey();
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController courseController = TextEditingController();
+
+  saveUser() {}
+
+  bool isLoading = false;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.user == null ? 'Add Student' : 'Edit Student'),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(24),
+          child: Form(
+            key: formkey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                TextFormField(
+                  controller: nameController,
+                  decoration: InputDecoration(
+                    labelText: 'Student Name',
+                    prefixIcon: Icon(Icons.person_outlined),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Please Enter a Name';
+                    }
+                    return null;
+                  },
+                  textInputAction: TextInputAction.next,
+                ),
+                const SizedBox(height: 20),
+                TextFormField(
+                  controller: courseController,
+                  decoration: InputDecoration(
+                    labelText: 'Course',
+                    prefixIcon: Icon(Icons.location_city_rounded),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Please Enter a Course';
+                    }
+                    return null;
+                  },
+                  textInputAction: TextInputAction.done,
+                  onFieldSubmitted: (_) => saveUser(),
+                ),
+                const SizedBox(height: 23),
+                ElevatedButton(
+                  onPressed: isLoading ? null : saveUser,
+                  child: isLoading
+                      ? SizedBox(
+                          width: 24,
+                          height: 24,
+                          child: CircularProgressIndicator(
+                            strokeWidth: 3,
+                            color: Colors.white,
+                          ),
+                        )
+                      : Text(
+                          widget.user == null
+                              ? 'Add Student'
+                              : 'Update Student',
+                        ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
